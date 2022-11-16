e8981ef0f1895e53f4630f07582c2bae8022a16c
Debmalya Jash <gkedeb@gmail.com>
2022-11-16 23:32:59 +0800

    daily

---
+++ src/main/java/stepik/Linked.java
@@ -0,0 +1,22 @@
+package stepik;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class Linked {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		Arrays.stream(str.split("\\s+")).map(Integer::valueOf).collect(Collectors.toCollection(LinkedList::new))
+				.stream().sorted(Comparator.reverseOrder()).skip(3).sorted(Comparator.naturalOrder())
+				.forEach(System.out::println);
+		Arrays.stream(reader.readLine().split(" ")).filter(x -> x.startsWith("J")).map(x -> x.substring(1))
+				.collect(Collectors.toCollection(LinkedList::new)).descendingIterator()
+				.forEachRemaining(System.out::println);
+	}
+}
